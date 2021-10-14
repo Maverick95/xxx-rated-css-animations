49d5291e62bb6139fcd4b391e695c792bcfdef60
Nick Emmerson <nickemmerson619@hotmail.com>
2021-10-14 09:38:56 +0100

    Create neon sign and funky spinning wheel with CSS and bare JS

---
+++ Index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+  
+  const id_toggle = 'spinner-toggle';
+  const id_target = 'spinner-target';
+  const class_anchor = 'spinner-anchor';
+  const classes_colour = ['red', 'white', 'blue'];
+  
+  let classes_colour_index = 0;
+  
+  document.getElementById(id_toggle)?.addEventListener('click', () => {
+      const spinner = document.createElement('div');
+      spinner.className = `spinner-anchor bg-${classes_colour[classes_colour_index++]}`;
+      classes_colour_index %= classes_colour.length;
+      document.getElementById(id_target)?.appendChild(spinner);
+  });
+
+});
