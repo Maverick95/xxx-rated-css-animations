d6ac626353dccc36bf4f614a64bcd5bdff4a8639
Nicholas Charles Emmerson <nickemmerson619@hotmail.com>
2021-12-05 20:13:54 +0000

    Beginning of changes - WIP

--- Index.js
+++ Index.js
@@ -6,12 +6,34 @@ document.addEventListener('DOMContentLoaded', () => {
   const classes_colour = ['red', 'white', 'blue'];
   
   let classes_colour_index = 0;
-  
+
   document.getElementById(id_toggle)?.addEventListener('click', () => {
       const spinner = document.createElement('div');
       spinner.className = `spinner-anchor bg-${classes_colour[classes_colour_index++]}`;
       classes_colour_index %= classes_colour.length;
       document.getElementById(id_target)?.appendChild(spinner);
+  });
+
+  
+  const id_toggle_curtains = 'curtains-toggle';
+  let toggle_curtains = false;
+  const id_target_curtains = 'curtains-target';
+
+  document.getElementById(id_toggle_curtains)?.addEventListener('click', () => {
+
+    if (!toggle_curtains) {
+      toggle_curtains = true;
+    }
+
+    const curtains = document.createElement('div');
+
+    
+
+
+
+      
+
+
   });
 
 });
