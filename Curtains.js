9fcefc06bb4f05b2c13c2e93d935d4b2c0d6ecf0
Nick Emmerson <nickemmerson619@hotmail.com>
2021-12-12 01:21:41 +0000

    Get implementation of curtains correct

--- Curtains.js
+++ Curtains.js
@@ -1,80 +1,75 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    let toggle_curtains = false;
+    const curtains_count = 5, cycle_count = 1;
 
-    const id_toggle_curtains = 'curtains-activate';
-    const id_target_curtains = 'curtains-target';
+    const curtains_toggle = document.getElementById('curtains-activate');
+    const curtains_target = document.getElementById('curtains-target');
 
-    document.getElementById(id_toggle_curtains)?.addEventListener('click', () => {
-
-        if (toggle_curtains) { return; }
-
-        toggle_curtains = true;
-
-        // Need to create the main container element, with position=relative.
-
-        const curtains_target = document.getElementById(id_target_curtains);
-
-        let curtains_count = 5;
-
-        const animationEndEventHandler = (isAnimationRunningForwards, index, count) => {
-
-            let evtFirstRun = true;
-            let evtForwards = isAnimationRunningForwards;
-                
-            return (target) => {
-                evtForwards = !evtForwards;
-                const timeout = 1000 * 
-                    (
-                        evtFirstRun ? index :
-                        (
-                            evtForwards ? (2 * index) + 1 : (2 * (count - index - 1)) + 1
-                        )
-                    );
-                evtFirstRun = false;
-                if (timeout > 0) {
-                    target.style.setProperty('animation-play-state', 'paused');
-                    setTimeout(() => {
-                        target.style.setProperty('animation-play-state', 'running');
-                    }, timeout);
-                }
-            };
-
-        };
-
-        if (curtains_target !== null) {
-
-            const curtains_container = document.createElement('div');
-            curtains_container.classList.add('curtains-container');
-            curtains_target.appendChild(curtains_container);
-
-            for (var i = 0; i < curtains_count; i++) {
-
-                const
-                    curtain_left = document.createElement('div'),
-                    curtain_right = document.createElement('div');
-
-                curtain_left.classList.add('curtain', 'curtain-left');
-                curtain_right.classList.add('curtain', 'curtain-right');
-
-                const isAnimationRunningForwards = false;
-
-                const transitionEventHandlerLeft = animationEndEventHandler(isAnimationRunningForwards, i, curtains_count);
-                const transitionEventHandlerRight = animationEndEventHandler(isAnimationRunningForwards, i, curtains_count);
-
-                curtain_left.addEventListener('animationiteration', (event) => { transitionEventHandlerLeft(event.target); });
-                curtain_right.addEventListener('animationiteration', (event) => { transitionEventHandlerRight(event.target); });
-                
-                transitionEventHandlerLeft(curtain_left);
-                transitionEventHandlerRight(curtain_right);
-
-                curtains_container.appendChild(curtain_left);
-                curtains_container.appendChild(curtain_right);
-
+    const toggleCurtainsStatus = (() => {
+        let toggle_curtains = false;
+        return () => {
+            toggle_curtains = !toggle_curtains;
+            if (toggle_curtains) {
+                curtains_toggle.setAttribute('disabled', 'true');
+            } else {
+                curtains_toggle.removeAttribute('disabled');
             }
+            return toggle_curtains;
+        };
+    })();
 
+    let curtains_finished = 0;
+
+    const createTransitionEventHandler = (index) => {
+
+        let animations = -1;
+        return (event) => {
+
+            if (++animations === (2 * cycle_count)) {
+                event.target.style.setProperty('animation-play-state', 'paused');
+                animations = -1;
+                if (++curtains_finished === (2 * curtains_count)) {
+                    toggleCurtainsStatus();
+                    curtains_finished = 0;
+                }
+            } else {
+                const timeout = 1000 *
+                (animations === 0 ? index :
+                (animations % 2 === 0 ? (2 * index) + 1 : (2 * (curtains_count - index - 1)) + 1));
+                if (timeout > 0) {
+                    event.target.style.setProperty('animation-play-state', 'paused');
+                    setTimeout(() => {
+                        event.target.style.setProperty('animation-play-state', 'running');
+                    }, timeout);
+                } else {
+                    event.target.style.setProperty('animation-play-state', 'running');
+                }
+            }
+        };
+    };
+
+    const curtains_container = document.createElement('div');
+    curtains_container.classList.add('curtains-container');
+    curtains_target.appendChild(curtains_container);
+
+    for (var i = 0; i < curtains_count; i++) {
+
+        for (var side of ['left', 'right']) {
+            const curtain = document.createElement('div');
+            curtain.classList.add('curtain', `curtain-${side}`);
+            const transitionHandler = createTransitionEventHandler(i);
+            curtain.addEventListener('animationiteration', transitionHandler);
+            curtain.style.setProperty('animation-play-state', 'paused');
+            curtains_container.appendChild(curtain);
         }
+    }
 
+    curtains_toggle.addEventListener('click', () => {
+        toggleCurtainsStatus();
+        const curtains = curtains_container.getElementsByClassName('curtain');
+        for (var c of curtains) {
+            c.dispatchEvent(new Event('animationiteration'));
+        }
     });
 
 });
