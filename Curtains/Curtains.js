5dfdc65c74adfb06e20a34b718cafe676e27751b
Nick Emmerson <nickemmerson619@hotmail.com>
2023-09-29 14:14:00 +0100

    Code tidy-up for better understanding

--- Curtains/Curtains.js
+++ Curtains/Curtains.js
@@ -23,35 +23,45 @@ document.addEventListener('DOMContentLoaded', () => {
     const createTransitionEventHandler = (index) => {
 
         let animations = -1;
-        return (event) => {
 
-            if (++animations === (2 * cycle_count)) {
+        const getAnimationDelaySeconds = () => {
+            if (animations === 0) {
+                // Animation start
+                return index;
+            }
+            else if (animations % 2) {
+                // Curtain has drawn again to leave stage open.
+                return (2 * (curtains_count - index - 1)) + 1;
+            }
+            // Curtain is closed covering stage.
+            return (2 * index) + 1;
+        };
+
+        return (event) => {
+            animations++;
+            if (animations === (2 * cycle_count)) {
                 event.target.style.setProperty('animation-play-state', 'paused');
                 animations = -1;
                 if (++curtains_finished === (2 * curtains_count)) {
                     toggleCurtainsStatus();
                     curtains_finished = 0;
                 }
-            } else {
-                const timeout = 1000 *
-                (animations === 0 ? index :
-                (animations % 2 === 0 ? (2 * index) + 1 : (2 * (curtains_count - index - 1)) + 1));
-                if (timeout > 0) {
-                    event.target.style.setProperty('animation-play-state', 'paused');
-                    setTimeout(() => {
-                        event.target.style.setProperty('animation-play-state', 'running');
-                    }, timeout);
-                } else {
+            }
+            else {
+                const timeout = 1000 * getAnimationDelaySeconds();
+                event.target.style.setProperty('animation-play-state', 'paused');
+                setTimeout(() => {
                     event.target.style.setProperty('animation-play-state', 'running');
-                }
+                }, timeout);
             }
         };
     };
 
+    // Lower-index curtains are behind higher-index curtains.
     for (var i = 0; i < curtains_count; i++) {
-
         for (var side of ['left', 'right']) {
             const curtain = document.createElement('div');
+            curtain.id = `curtain-${side}-${i}`;
             curtain.classList.add('curtain', `curtain-${side}`);
             const transitionHandler = createTransitionEventHandler(i);
             curtain.addEventListener('animationiteration', transitionHandler);
